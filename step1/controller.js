806902a26c0973b9bbe087f51ddca74f60ba2ec6
Facundo Crego <facundo.crego@gmail.com>
2015-08-08 00:18:00 -0300

    add jseditor

--- step1/controller.js
+++ step1/controller.js
@@ -1,6 +1,30 @@
 angular.module('app')
 .controller('Step1Ctrl', function($scope, Promise, $timeout, $mdSidenav) {
     $timeout(Prism.highlightAll);
+
+    window.Q = function() {
+        var _then;
+        this.then = function (callback) {
+            _then = callback;
+        };
+        this.resolve = function (value) {
+            _then(value);
+        };
+    };
+    window.AsyncAction = function (action) {
+        return function () {
+            var args = Array.prototype.slice.call(arguments);
+            var promise = new Q();
+            setTimeout(function() {
+                var value = action.apply(this, args);
+                promise.resolve(value);
+            },1000);
+            return promise;
+        };
+    };
+    window.multiply = AsyncAction(function(arg, arg1) {
+        return arg*arg1;
+    });
 });
 
 
--- step2/controller.js
+++ step2/controller.js
@@ -1,6 +1,40 @@
 angular.module('app')
 .controller('Step2Ctrl', function($scope, Promise, $timeout, $mdSidenav) {
     $timeout(Prism.highlightAll);
+
+    window.Q = function() {
+        var _then, _reject;
+        this.then = function(callback, reject) {
+            _then = callback;
+            _reject = reject;
+        };
+        this.resolve = function(value) {
+            _then(value);
+        };
+        this.reject = function(error) {
+            _reject(error);
+        };
+    };
+    window.AsyncAction = function(action) {
+        return function(){
+            var args = Array.prototype.slice.call(arguments);
+            var promise = new Q();
+            setTimeout(function() {
+                try {
+                  var value = action.apply(this, args);
+                  promise.resolve(value);
+                }
+                catch (e) {
+                  promise.reject(e);
+                }
+            },1000);
+            return promise;
+        };
+    };
+
+    window.multiply = AsyncAction(function(arg, arg1) {
+        throw "An error";
+    });
 });
 
 
